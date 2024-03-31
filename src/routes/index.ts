c9785d4882947bc8711938d97c91784f676dd443
dhrumil-7span <dhrumil@7span.com>
2024-03-31 18:43:25 +0530

    updated changes and added jwt file indexroute

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 const router = express()
 
 import bookingRoute from './bookingRoute'
+import customerRoute from './customerRoute'
 
 router.use("/booking",bookingRoute)
+router.use("/user",customerRoute)
 
 
 export default router
--- src/routes/indexRoute.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express()
-import customerRoute from './customerRoute'
-
-router.use("/user",customerRoute)
-
-export default router
