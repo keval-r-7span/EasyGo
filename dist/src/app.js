1b7a30cc13f563879d3158b00dfe462aea77c83d
keval Rabadiya <kevalrabadiya27@gmail.com>
2024-04-02 17:27:36 +0530

    updated

--- dist/src/app.js
+++ dist/src/app.js
@@ -3,12 +3,14 @@ import connectDB from './configs/dbConnection';
 import indexRoute from './routes/index';
 import { PORT } from './helper/constants';
 import logger from './utils/logger';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+app.use(cors());
 // Define routes
 app.use("/api/v1", indexRoute);
 app.get("/", (req, res) => {
-    res.status(200).send("Welcome to RideWithMe API");
+    res.status(200).send("🚀Welcome to RideWithMe-API 🚕..");
 });
 // Connect to MongoDB Atlas .
 connectDB();
