a8412779c5f81f59b425ecef95897d5d16a984c7
keval Rabadiya <kevalrabadiya27@gmail.com>
2024-04-03 11:55:28 +0530

    payment model add

---
+++ src/validation/bookingValidation.ts
@@ -0,0 +1,30 @@
+import { NextFunction,Request,Response } from 'express';
+import Joi from 'joi';
+
+const bookingJoiSchema= Joi.object({
+  customer:Joi.string(),
+  driver:Joi.string(),
+  vehicle:Joi.string(),
+  vehicleClass:Joi.string(),
+  pickupLocation: Joi.string().min(3).max(100).required(),
+  dropoffLocation: Joi.string().min(3).max(100).required(),
+  pickupTime: Joi.string().required().default(Date.now()),
+  status: Joi.string().lowercase(),
+  fare: Joi.number().required(),
+  rating: Joi.number(),
+  comments: Joi.string(),
+});
+
+const paymentJoiSchema = Joi.object({
+  
+})
+
+const validateRequest = (req:Request, res:Response, next:NextFunction) => {
+  const { error } = bookingJoiSchema.validate(req.body);
+  if (error) {
+      return res.status(400).json({sucess:false,error: `JoiSchema validation error: ${error.details[0].message}`});
+  }
+  next();
+};
+
+export default validateRequest;
