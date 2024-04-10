16c514307b617c3c317cdccfcdec4fcb9ffb38f2
keval Rabadiya <kevalrabadiya27@gmail.com>
2024-04-10 11:06:50 +0530

    updated

--- src/app.ts
+++ src/app.ts
@@ -9,17 +9,17 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 // Define routes
-// app.use("/api/v1", indexRoute);
+app.use("/api/v1", indexRoute);
 
 app.get("/",(req:Request,res:Response)=>{
   res.status(200).send("🚀Welcome to RideWithMe-API 🚕..")
 })
 // Connect to MongoDB Atlas .
 connectDB();
-// if (parseInt(process.version.slice(1), 10) < 16) {
-//   logger.error('Node.js version 16 or higher is required to run this application.');
-//   process.exit(1); 
-// }
+if (parseInt(process.version.slice(1), 10) < 16) {
+  logger.error('Node.js version 16 or higher is required to run this application.');
+  process.exit(1); 
+}
 // Start the server
 app.listen(PORT, ():void => {
   logger.info(`🚀 Server is running.. on http://localhost:${PORT}🚀..`);
