b8b33fb3ec78449bf3234ebc7e4f6391682e6565
keval Rabadiya <kevalrabadiya27@gmail.com>
2024-04-16 14:52:45 +0530

    fix small bug

--- src/validation/bookingValidation.ts
+++ src/validation/bookingValidation.ts
@@ -9,7 +9,6 @@ const schemas:Record<string,Schema> = {
 const validateData = (model:string,data:any):ValidationResult=>{
   const schema = schemas[model];
     if (!schema) {
-        logger.error("Schema not found..");
         throw new Error("Schema not found for validation..")
     }
     return schema.validate(data)
