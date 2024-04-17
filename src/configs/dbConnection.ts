2f0c6bb93f50ac3b71a659bd0594cfeb89beec75
keval Rabadiya <kevalrabadiya27@gmail.com>
2024-04-17 17:06:42 +0530

    fix small bug

--- src/configs/dbConnection.ts
+++ src/configs/dbConnection.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(DB_DATA.DB_URL);
+    await mongoose.connect(DB_DATA.URL);
     logger.info("Connected to MongoDB Atlas");
   } catch (error) {
     logger.error("Error connecting to MongoDB Atlas:", error);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
-const router = express()
-
 import bookingRoute from './bookingRoute'
+const router = express()
 
 router.use("/booking",bookingRoute)
 
