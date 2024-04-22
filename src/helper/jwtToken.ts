e4c79cab56888a4d06e0cb713e87263f230be666
keval Rabadiya <kevalrabadiya27@gmail.com>
2024-04-22 15:36:10 +0530

    eslint and prettier added

--- src/helper/jwtToken.ts
+++ src/helper/jwtToken.ts
@@ -1,15 +1,10 @@
 import jwt from 'jsonwebtoken'
 import { JWT } from "./constants";
 
-const generateAccessToken = (user: { _id: any; phoneNumber: string; email: string; role: string; }) => {
-  const payload = {
-    _id:  user._id,
-    phoneNumber: user.phoneNumber,
-    email: user.email,
-    role: user.role,
-  };
+const generateAccessToken = (user:object) => {
+  
   const options = { expiresIn: JWT.EXPIRES };
-  return jwt.sign(payload, JWT.SECRET, options);
+  return jwt.sign(user, JWT.SECRET, options);
 };
 
 export default generateAccessToken;
