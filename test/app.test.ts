088da8a201a023809db8ad40a5c0a91d2f0ae2da
dhrumil-7span <dhrumil@7span.com>
2024-05-17 15:25:41 +0530

    updated

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,5 +1,5 @@
 import supertest from "supertest";
-import { app } from "../src/app";
+import  app  from "../src/app";
 import { setupDB } from './configs/dbConnection';
 
 describe('GET /', () => {
