91fc39c615f70c88a0f4e4bd3d634a9fa08cb82d
dhrumil-7span <dhrumil@7span.com>
2024-05-17 15:25:41 +0530

    updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express()
 import customerRoute from './customerRoute'
 import paymentRoute from './paymentRoute'
+import driverRoute from './driverRoute';
 
 router.use('/user',customerRoute)
 router.use('/driver',driverRoute)
