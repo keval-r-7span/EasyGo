8ee0b3c4386f2ca772fce4d829c0fcea905f38ff
keval Rabadiya <kevalrabadiya27@gmail.com>
2024-05-20 11:00:03 +0530

    split into master

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,7 @@
 import express from 'express';
 const router = express()
-import customerRoute from './customerRoute'
 import paymentRoute from './paymentRoute'
-import driverRoute from './driverRoute';
 
-router.use('/user',customerRoute)
-router.use('/driver',driverRoute)
 router.use('/payment',paymentRoute)
 
 export default router;
