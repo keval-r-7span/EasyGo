4d8a7fdc1582adc4c0f0fa9107f8a6011091d949
kevalrabadiya <kevalrabadiya27@gmail.com>
2024-05-27 11:29:17 +0530

    remove middleware

--- src/routes/bookingRoute.ts
+++ src/routes/bookingRoute.ts
@@ -2,10 +2,9 @@ import { Router } from "express";
 const router = Router();
 import {viewBooking,updateBooking,deleteBooking,createBooking} from '../controllers/bookingController'
 import {validateRequest} from '../validation/joiValidation';
-import { verifyToken } from "../middleware/authMiddleware";
 
-router.get('/list',verifyToken,viewBooking)
-router.post('/',verifyToken,validateRequest,createBooking)
+router.get('/list',viewBooking)
+router.post('/',validateRequest,createBooking)
 router.put('/:id',updateBooking)
 router.delete('/:id',deleteBooking)
 
